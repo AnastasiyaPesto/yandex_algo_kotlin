55d9f1617a559112a61b21cf196243db4f774cf2
Anastasiia Zentsova <pestovnikova2013@gmail.com>
2023-12-30 17:32:14 +0300

    sprint1: H

--- src/main/kotlin/ru/zentsova/yandex/sprint1/H.kt
+++ src/main/kotlin/ru/zentsova/yandex/sprint1/H.kt
@@ -1,2 +1,41 @@
 package ru.zentsova.yandex.sprint1
 
+// https://contest.yandex.ru/contest/22449/problems/H/?success=104101690#2989/2020_04_13/vTLkp8umJC - H. Двоичная система
+import kotlin.text.StringBuilder
+
+fun main() {
+	val number1 = readln()
+	val number2 = readln()
+	println(getSum(number1, number2))
+}
+
+private fun getSum(firstNumber: String, secondNumber: String): String {
+	var longerNumber = StringBuilder(firstNumber)
+	var shorterNumber = StringBuilder(secondNumber)
+
+	if (firstNumber.length < secondNumber.length) {
+		longerNumber = StringBuilder(secondNumber)
+		shorterNumber = StringBuilder(firstNumber)
+	}
+
+	for (i in 0 until (longerNumber.length - shorterNumber.length)) {
+		shorterNumber.insert(0, "0")
+	}
+
+	var carry = 0
+	val sb = StringBuilder()
+	for (i in longerNumber.length - 1 downTo 0) {
+		var c = longerNumber[i].digitToInt() + shorterNumber[i].digitToInt() + carry
+		if (c > 1) {
+			c -= 2
+			carry = 1
+		} else {
+			carry = 0
+		}
+		sb.insert(0, c)
+	}
+
+	if (carry == 1) sb.insert(0, 1)
+
+	return sb.toString()
+}
