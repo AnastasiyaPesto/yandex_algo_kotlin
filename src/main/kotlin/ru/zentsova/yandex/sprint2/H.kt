e82b6689451ad976c22b67350cad0047c905af40
Anastasiia Zentsova <pestovnikova2013@gmail.com>
2024-01-16 07:56:47 +0300

    sprint 2 - H

--- src/main/kotlin/ru/zentsova/yandex/sprint2/H.kt
+++ src/main/kotlin/ru/zentsova/yandex/sprint2/H.kt
@@ -2,22 +2,22 @@ package ru.zentsova.yandex.sprint2
 
 class Node<V>(var value: V, var next: Node<V>? = null, var prev: Node<V>? = null)
 
-class Stack(var cur: Node<Char>) {
+class Stack(var cur: Node<Char>? = null) {
 
   fun push(char: Char) {
     if (cur == null) {
       cur = Node(value = char)
     } else {
       cur = Node(value = char, prev = cur)
-      cur.prev!!.next = cur
+      cur!!.prev!!.next = cur
     }
   }
 
   fun pop(): Char? {
     if (cur == null) return null
 
-    val ch = cur.value
-    cur = cur.prev!!
+    val ch = cur!!.value
+    cur = cur!!.prev
 
     return ch
   }
@@ -30,6 +30,30 @@ class Stack(var cur: Node<Char>) {
 val CLOSE_BRACKETS = setOf('}', ')', ']')
 val CLOSE_OPEN_BRACKET_PAIRS = mapOf('}' to '{', ')' to '(', ']' to '[')
 
-fun isCorrectBracketSeq(seq: String) {
 
+fun main() {
+  val isCorrect = isCorrectBracketSeq(readln())
+  if (isCorrect) println("True") else println("False")
+}
+
+private fun isCorrectBracketSeq(seq: String): Boolean {
+  if (seq == null)
+    return false
+
+  if (seq.isEmpty())
+    return true
+
+  val stack = Stack()
+  for (i in seq.indices) {
+    val ch = seq[i]
+    if (CLOSE_BRACKETS.contains(ch)) {
+      val openBracket = CLOSE_OPEN_BRACKET_PAIRS[ch]
+      val firstCh = stack.pop()
+      if (firstCh == null || firstCh != openBracket) return false
+    } else {
+      stack.push(ch)
+    }
+  }
+
+  return stack.isEmpty()
 }
