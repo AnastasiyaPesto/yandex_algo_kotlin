785ae734983fd5efa7970f5aa93534ee86a134ba
Anastasia Zentsova <a.zentsova@lipt-soft.ru>
2024-01-26 21:26:17 +0300

    Sprint 3 - L

--- src/main/kotlin/ru/zentsova/yandex/sprint3/L.kt
+++ src/main/kotlin/ru/zentsova/yandex/sprint3/L.kt
@@ -10,41 +10,48 @@ fun main() {
 	val bikePrice = reader.readInt()
 	val doubleBikePrice = 2 * bikePrice
 
-	val day1 = binarySearch(moneybox.toIntArray(), bikePrice)
-	val day2 = binarySearch(moneybox.toIntArray(), doubleBikePrice)
+	val day1 = binarySearch(moneybox, bikePrice)
+	val day2 = binarySearch(moneybox, doubleBikePrice)
 	println("${if (day1 == -1) -1 else day1 + 1} ${if (day2 == -1) -1 else day2 + 1}")
 }
 
-fun binarySearch(arr: IntArray, x: Int): Int {
+fun binarySearch(arr: List<Int>, x: Int): Int {
 	var left = 0
 	var right = arr.size - 1
-	var res = -1
+	var mid = -1
+	var isEqualFound = false
 
-	while (left < right) {
-		var mid = left + (right - left) / 2
-		if (arr[mid] == x) {
-			res = mid
-			break
-		} else if (right - left == 1) {
-			res = if (arr[left] == x) left else right
-			break
-		} else if (arr[mid] > x) {
+	// Ищем до тех пор, пока не останется обработать два элемента
+	while (right - left != 1) {
+		mid = left + (right - left) / 2
+		if (arr[mid] > x) {
 			right = mid
-		} else {
+		} else if (arr[mid] < x) {
 			left = mid
+		} else {
+			// если сразу нашли, то нужно найти самый первый индекс с таким значением, значит двигаемся влево
+			isEqualFound = true
+			break
 		}
 	}
 
-	res = if (arr[left] == x) left else right
-
-	if (arr[res] < x)
-		return -1
-
-	if (res > 0) {
-		val num = arr[res]
-
-		while (arr[res - 1] == num) {
-			 res--
+	val res = if (isEqualFound) {
+		if (arr[0] == x) {
+			0
+		} else {
+			while (mid != 0 && arr[mid - 1] == x) {
+				mid--
+			}
+			mid
+		}
+	} else {
+		// если сразу не нашли, то обрабатываем два элемента
+		// now there are only 2 elements
+		when {
+			arr[left] < x && x < arr[right] -> right
+			x == arr[left] -> left
+			x == arr[right] -> right
+			else -> -1
 		}
 	}
 
